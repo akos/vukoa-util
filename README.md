1a06f2d4933e5e01f47d78e3f7692950ae2ca37e
林冠夫 <lgfshr@163.com>
2018-02-12 17:19:04 +0800

    :construction: update util

---
+++ README.md
@@ -0,0 +1,3 @@
+# vukoa-util
+The `util` module is improve more methods to check type for node, extend core-util-isfor node, extend core-util-is
+
--- test/test.js
+++ test/test.js
@@ -1,5 +1,12 @@
 const util = require("../index");
+const what = util.isWhat;
 
 var id = '532524198901290018w';
 var result = util.isCardID(id);
+console.log(result);
+var result = what.isString(id);
+console.log(result);
+var result = what.isNumber(id);
+console.log(result);
+var result = what.isFalse(null);
 console.log(result);
